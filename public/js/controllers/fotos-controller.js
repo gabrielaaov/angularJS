e495883082564863b29fbf682829d97d6f475a64
Gabi <gabriela@seegma.com.br>
2015-10-08 17:38:22 -0300

    rotas

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,20 +1,8 @@
 angular.module('alurapic').controller('FotosController' , function($scope, $http){ //agora estou usando o modulo alurapic, e pedindo para criar um controller 'FotosConroller' (letra maiuscula sempre) e uma funcao q o define
 //uso o $scope dentro da funcao pq senao, não será possível acessar os elementos dentro da funcao, o js nao permite fica tudo em vegas
 
-	$scope.fotos =[
-		// {
-		// 	titulo: 'Bridge1',
-		// 	url: 'http://lorempixel.com/output/city-q-c-640-480-9.jpg'
-		// },
-		// {
-		// 	titulo: 'Bridge2',
-		// 	url: 'http://lorempixel.com/output/city-q-c-640-480-9.jpg'
-		// },
-		// {
-		// 	titulo: 'Bridge3',
-		// 	url: 'http://lorempixel.com/output/city-q-c-640-480-9.jpg'
-		// }
-	];
+	$scope.fotos =[];
+	$scope.filtro='';
 
 	$http.get('/v1/fotos')
 	.success(function(retorno){
@@ -24,11 +12,5 @@ angular.module('alurapic').controller('FotosController' , function($scope, $http
 	.error(function(erro){
 		console.log(erro);
 	});
-
-	// promise.then(function(retorno){
-	// 	$scope.fotos = retorno.data;
-	// }).catch(function(error){
-	// 	console.log(error);
-	// });
 
 }); 
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
-angular.module('alurapic', []); //criei o modulo alurapic, como estou criando obrigatoriamente passo um segundo parametro vazio mesmo.
+angular.module('alurapic', ['minhasDiretivas','ngAnimate', 'ngRoute'])
+
+.config(function($routeProvider, $locationProvider){
+
+	$locationProvider.html5Mode(true);
+	$routeProvider.when('/fotos',{
+		templateUrl: 'partials/principal.html',
+		controller: 'FotosController'
+	});
+
+	$routeProvider.when('/fotos/new',{
+		templateUrl: 'partials/foto.html'
+	});
+
+	$routeProvider.otherwise({redirectTo:'/fotos'});
+
+}); 
