943adedd6b7635de6340114162cfa125c6bd4ce1
Gabi <gabriela@seegma.com.br>
2015-10-09 17:55:57 -0300

    aula 09 - E se o nosso back-end mudasse

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,6 +1,12 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams){
+angular.module('alurapic').controller('FotoController', function($scope, recursoFoto, $routeParams){
 	$scope.foto = {};
 	$scope.mensagem = '';
+
+	// var recursoFoto = $resource('/v1/fotos/:fotoId' , null , {
+	// 	update:{
+	// 		method:'PUT'
+	// 	}
+	// });
 
 	if($routeParams.fotoId){
 		$http.get('v1/fotos/' + $routeParams.fotoId)
@@ -18,14 +24,21 @@ angular.module('alurapic').controller('FotoController', function($scope, $http, 
 		if($scope.formulario.$valid){
 
 			if($scope.foto._id){//se minha foto tiver um id (já tiver sido cadastrada eu altero aqui)
-				$http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
-				.success(function(){
+
+				recursoFoto.update({fotoId: $scope.foto._id}, $scope.foto, function(){
 					$scope.mensagem = 'A foto foi alterada com sucesso';
-				})
-				error(function(erro){
+				},function(erro){
 					console.log(erro);
 					$scope.mensagem = 'Não foi possível alterar a foto';
-				})
+				});
+				// $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+				// .success(function(){
+				// 	$scope.mensagem = 'A foto foi alterada com sucesso';
+				// })
+				// error(function(erro){
+				// 	console.log(erro);
+				// 	$scope.mensagem = 'Não foi possível alterar a foto';
+				// })
 			}else{//senao eu cadastro
 
 				$http.post('v1/fotos', $scope.foto)
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,31 +1,53 @@
-angular.module('alurapic').controller('FotosController' , function($scope, $http){ //agora estou usando o modulo alurapic, e pedindo para criar um controller 'FotosConroller' (letra maiuscula sempre) e uma funcao q o define
+angular.module('alurapic').controller('FotosController' , function($scope, recursoFoto){ //agora estou usando o modulo alurapic, e pedindo para criar um controller 'FotosConroller' (letra maiuscula sempre) e uma funcao q o define
 //uso o $scope dentro da funcao pq senao, não será possível acessar os elementos dentro da funcao, o js nao permite fica tudo em vegas
 
 	$scope.fotos =[];
 	$scope.filtro='';
 	$scope.mensagem = '';
 
-	$http.get('/v1/fotos')
-	.success(function(retorno){
-		console.log(retorno)
-		$scope.fotos= retorno;
-	})
-	.error(function(erro){
+	// var recursoFoto = $resource('v1/fotos/:fotoId');
+
+	recursoFoto.query(function(fotos){
+		$scope.fotos=fotos;
+	}, function(erro){
 		console.log(erro);
 	});
 
-	$scope.remover = function(foto){
-		//remove a foto
-		$http.delete('v1/fotos/' + foto._id)
-		.success(function(){
+
+	// $http.get('/v1/fotos')
+	// .success(function(retorno){
+	// 	console.log(retorno)
+	// 	$scope.fotos= retorno;
+	// })
+	// .error(function(erro){
+	// 	console.log(erro);
+	// });
+	$scope.remover=function(foto){
+
+		recursoFoto.delete({fotoId : foto._id}, function(){
 			var indiceFoto = $scope.fotos.indexOf(foto);
 			$scope.fotos.splice(indiceFoto, 1);
 			$scope.mensagem = 'Foto' + foto.titulo + ' foi removida com sucesso';
-		})
-		.error(function(erro){
+		}, function(erro){
 			console.log(erro);
 			$scope.mensagem = 'Não foi possível remover a foto' + foto.titulo;
 		});
+
 	};
+
+
+	// $scope.remover = function(foto){
+	// 	//remove a foto
+	// 	$http.delete('v1/fotos/' + foto._id)
+	// 	.success(function(){
+	// 		var indiceFoto = $scope.fotos.indexOf(foto);
+	// 		$scope.fotos.splice(indiceFoto, 1);
+	// 		$scope.mensagem = 'Foto' + foto.titulo + ' foi removida com sucesso';
+	// 	})
+	// 	.error(function(erro){
+	// 		console.log(erro);
+	// 		$scope.mensagem = 'Não foi possível remover a foto' + foto.titulo;
+	// 	});
+	// };
 
 }); 
